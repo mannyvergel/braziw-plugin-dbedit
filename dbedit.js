826a2a94e106096917ab792f4cd5f8905620712c
Manny <manny@mvergel.com>
2016-10-27 17:16:50 +0800

    Autoscan models dir and add to menu option

--- dbedit.js
+++ dbedit.js
@@ -1,9 +1,13 @@
+var dbeditUtils = require('./utils/dbeditUtils.js');
+var fs = require('fs');
+
 module.exports = function DbEdit(pluginConf, web, next) {
   var pluginPath = pluginConf.pluginPath;
   pluginConf = web.utils.extend({
       saveView: pluginPath + "/views/dbedit-save.html",
       collectionsView: pluginPath + "/views/dbedit-collections.html",
       listView: pluginPath + "/views/dbedit-list.html",
+      addToMenu: true,
     },
     pluginConf);
 
@@ -14,21 +18,26 @@ module.exports = function DbEdit(pluginConf, web, next) {
 
   web.applyRoutes(require('./conf/routes.js'));
 
-  web.cms.adminMenu.push({
-    headerText:'DB Editor',
-    permissions: ['ADMIN'],
-    items:[
-      { text: 'Collections', link:'/admin/dbedit/collections'},
-    ]
-  })
+  if (pluginConf.addToMenu) {
+    web.cms.adminMenu.push({
+      headerText:'DB Editor',
+      permissions: ['ADMIN'],
+      items:[
+        { text: 'Collections', link:'/admin/dbedit/collections'},
+      ]
+    })
+  }
 
-  normaliseModels();
+  setupModels();
 
   next();
 }
 
-function normaliseModels() {
+function setupModels() {
+
   var modelInfos = web.cms.dbedit.conf.models;
+  scanModelsDir(modelInfos);
+
   for (var i in modelInfos) {
     var modelInfo = modelInfos[i];
     if (!modelInfo.name) {
@@ -39,10 +48,24 @@ function normaliseModels() {
 
 function getModelNameFromPath(modelPath) {
   if (!modelPath) {
-    return '';
+    return null;
   }
 
   var arrModelSplit = modelPath.split('/');
   var nameWithJs = arrModelSplit[arrModelSplit.length-1];
   return nameWithJs.split('.')[0];
+}
+
+function scanModelsDir(modelInfos) {
+  var files = fs.readdirSync(web.conf.baseDir + web.conf.modelsDir);
+  for (var i in files) {
+    var file = files[i];
+
+    var modelName = getModelNameFromPath(file);
+
+    if (!dbeditUtils.getModelConf(modelName) && web.stringUtils.endsWith(file, '.js')) {
+      modelInfos.push({path: modelName});
+    }
+  }
+
 }
